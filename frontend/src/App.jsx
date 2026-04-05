8e8d389410a5610c41fdd9c8be7b397eb9426e75
Paras Jain <parasjain08803@gmail.com>
2026-04-06 00:19:18 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,6 @@ export default function App() {
 
     const data = await response.json();
 
-    console.log(data)
 
     if (!data.is_resume) {
       setUploadStatus(data.message || "Uploaded file is not a valid resume.");
