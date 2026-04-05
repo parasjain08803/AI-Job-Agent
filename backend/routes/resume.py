cd1df86449b94962ccca91e597f1f6d2ba7a55af
Paras Jain <parasjain08803@gmail.com>
2026-04-05 22:55:33 +0530

    .

--- backend/routes/resume.py
+++ backend/routes/resume.py
@@ -3,6 +3,7 @@ from loaders.resume_loader import load_resume
 from llms.prompts import classifier_prompt,parser_prompt
 from llms.llm import classifier_llm,parser_llm
 from services.resume_service import process_resume
+from langchain_core.output_parsers import StrOutputParser
 
 router = APIRouter(prefix="/resume")
 
@@ -18,18 +19,26 @@ async def upload_resume(file: UploadFile = File(...)):
 
     first_page_text = documents[0].page_content
 
-    classification_chain = classifier_prompt | classifier_llm
+    classification_parser=StrOutputParser()
+
+    classification_chain = classifier_prompt | classifier_llm | classification_parser
     classification = classification_chain.invoke({"text": first_page_text})
 
     result = classification.content.strip().lower()
 
-    if "resume" not in result:
+    if result == "not_resume":
         return {
             "is_resume": False,
             "message": "This is not a resume. Please upload a valid resume."
         }
 
     final_output=process_resume(documents)
+
+    if not final_output.get("skills",[]):
+        return {
+            "is_resume": False,
+            "message": "This is not a resume. Please upload a valid resume."
+        }
 
     return {
         "is_resume": True,
