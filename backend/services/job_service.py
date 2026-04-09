804bb8237d22d012e0813b3922f4579b92f1794f
Paras Jain <parasjain08803@gmail.com>
2026-04-09 23:15:48 +0530

    .

--- backend/services/job_service.py
+++ backend/services/job_service.py
@@ -16,7 +16,7 @@ async def fetch_jobs_async(
 
     tasks.append(fetch_jsearch(query, location, page=1,job_type=job_type,remote=remote))
 
-    tasks.append(fetch_internshala(query,location,remote,pages=1,job_type=job_type))
+    ##tasks.append(fetch_internshala(query,location,remote,pages=1,job_type=job_type))
 
     results = await asyncio.gather(*tasks, return_exceptions=True)
 
