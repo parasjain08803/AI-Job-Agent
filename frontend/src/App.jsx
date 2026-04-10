d1d4187456ad8794576a458de4d1fa59c464b468
Paras Jain <parasjain08803@gmail.com>
2026-04-11 02:06:47 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Select from "react-select";
 import locationOptions from "./data/locations";
 
-const API_BASE =  "https://aijobagent.duckdns.org";
+const API_BASE = "https://aijobagent.duckdns.org";
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 24 },
