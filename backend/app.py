2dbb7db6432c16e05057776485e761b60145866b
Paras Jain <parasjain08803@gmail.com>
2026-04-12 04:08:09 +0530

    .

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,3 @@
-import asyncio
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import resume
